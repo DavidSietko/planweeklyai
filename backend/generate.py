01be115bca1470e0189af6831d658918e1a0cfe1
unknown <mypuzzle12@gmail.com>
2025-07-16 19:55:12 +0100

    added skeleton code for ai content generation, working connection to openrouter

--- backend/generate.py
+++ backend/generate.py
@@ -3,11 +3,27 @@ from os import getenv
 from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
 from utils import get_db_connection
+from openai import OpenAI
 
 load_dotenv()
 
 router = APIRouter()
 
 @router.get("/generate/schedule")
-def generate_schedule(request: Request):
-    return {"message": "Hello, World!"}
+def generate_schedule():
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=getenv("OPENROUTER_API_KEY")
+    )
+
+    completion = client.chat.completions.create(
+        extra_body={},
+        model="moonshotai/kimi-k2:free",
+        messages=[
+            {
+                "role": "user",
+                "content": "What is the meaning of life?"
+            }
+        ]
+    )
+    return completion.choices[0].message.content
