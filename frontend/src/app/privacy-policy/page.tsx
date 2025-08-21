efb1697e19513b2879668ba6467489850afa64b3
unknown <mypuzzle12@gmail.com>
2025-08-21 15:37:52 +0100

    finished privacy policy page

--- frontend/src/app/privacy-policy/page.tsx
+++ frontend/src/app/privacy-policy/page.tsx
@@ -5,13 +5,21 @@ export default function PrivacyPolicyPage() {
     <div className={styles.page}>
       <div className={styles.content}>
         <h1>Privacy Policy</h1>
-        <div>
-          <h2>Information We Collect</h2>
-          <p>he only information we ask of users at planweekly ai is email and google calender access.
-          We use the email to create accounts to uniquely identify users. We then ask for calender read and write access associated with this email.</p>
+        <hr className={styles.Hr} />
+        <div className={styles.contentBox}>
+          <h2 className={styles.title}>Information We Collect</h2>
+          <p>We collect your email address to create your account and identify you uniquely.</p>
+          <p>Google Calendar Access: With your permission, we request read and write access to your Google Calendar to generate and optimize your weekly schedule. 
+            You can revoke these permissions at any time, however you will be unable to generate schedules.</p>
         </div>
-        <div>
-          <h3>Data Sharing</h3>
+        <hr className={styles.Hr} />
+        <div className={styles.contentBox}>
+          <h2 className={styles.title}>How we use Information</h2>
+          <p>To provide personalized scheduling services. To maintain and secure your account.</p>
+        </div>
+        <hr className={styles.Hr} />
+        <div className={styles.contentBox}>
+          <h2 className={styles.title}>Data Sharing</h2>
           <p>We do not share any user data with third parties. You are also able to delete your account at any time, which will remove all data associated with it.</p>
         </div>
       </div>
